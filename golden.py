5eb0b9c9cbda0c1114d62d322da672e8494567fb
unknown <morokataben.j@gmail.com>
2019-12-21 20:31:45 +0900

    Update: make function to draw a rectangle.

--- golden.py
+++ golden.py
@@ -4,17 +4,21 @@ from matplotlib import pyplot as plt
 from matplotlib import patches
 
 
+def draw_square(xy, w, h, angle):
+    return patches.Rectangle(xy, w, h, angle, ec='k', fill=False)
+
+
 def main():
     print("Draw fractal.")
 
     fig = plt.figure(figsize=(16, 12))
     ax1 = fig.add_subplot(111)
-    # patches.Rectangle((0.0, 0.0), 1, 1)
-    rectangle = patches.Rectangle((-0.5, -0.5), 1.0, 1.0, ec='k', fill=False)
+
+    rectangle = draw_square((-0.5, 0.5), 1.0, 1.0, -90)
     ax1.add_patch(rectangle)
 
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
 
     ax1.set_aspect('equal')
 
