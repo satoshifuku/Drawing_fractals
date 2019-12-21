b82826a147b5bf94156ba6912815a1b527b31799
unknown <morokataben.j@gmail.com>
2019-12-22 00:40:38 +0900

    Fix: way to draw a rectangle without function of matplotlib

--- golden.py
+++ golden.py
@@ -4,8 +4,6 @@ from matplotlib import pyplot as plt
 from matplotlib import patches
 
 
-
-
 def rot_matrix(angle):
     return np.array([[math.cos(angle), -math.sin(angle)],[math.sin(angle),math.cos(angle)]])
 
@@ -15,38 +13,39 @@ def golden_ratio():
 
 
 def draw_square(xy, w, h, angle):
-    return patches.Rectangle(xy, w, h, angle, ec='k', fill=False)
+    xs = [xy[0], xy[0] ,xy[0] + w ,xy[0] + w,xy[0]]
+    ys = [xy[1], xy[1] + h, xy[1] + h ,xy[1] ,xy[1]]
+    return plt.plot(xs,ys, linestyle="-")
+    # return plt.Rectangle(xy, w, h, angle, ec='k', fill=False)
 
 
 def main():
     print("Draw fractal.")
     
     fig = plt.figure(figsize=(16, 12))
-    ax1 = fig.add_subplot(111)
+    plt.axes().set_aspect('equal')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
 
-    xy = np.c_[np.array([0.0, 0.0])]
-    v = np.c_[np.array([2.0, 2.0])] 
+    n = 10
+    xy = np.c_[np.array([-2.0, -2.0])]
+    v = np.c_[np.array([4.0, 4.0])] 
     currentAngle = 0
-    for i in range(10):
+    
+    for i in range(n):
         print("--------------")
-        # print(xy)
 
         v = np.dot(rot_matrix(currentAngle) , v)
         print(v)        
 
         rectangle = draw_square((xy[0], xy[1]), v[0], v[1], 0)
-        ax1.add_patch(rectangle)
+        # imgs.append(plt.axes().add_patch(rectangle))
 
         xy = xy + v
         v = golden_ratio() * v
         currentAngle = -0.5 * math.pi
 
     
-    plt.xlim(-4, 4)
-    plt.ylim(-4, 4)
-
-    ax1.set_aspect('equal')
-
     fig.savefig('img.png', dpi=800)
     print('finished')
   
